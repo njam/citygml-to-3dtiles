65b36448fbfcd308f76a0093b3689ca0eff8a2e6
Reto Kaiser <reto@retokaiser.com>
2018-03-25 16:25:43 +0200

    Add CLI script

---
+++ bin/citygml-to-3dtiles.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/node --experimental-modules
+
+import caporal from 'caporal'
+import Converter from "../src/Converter.mjs";
+import fs from "fs";
+
+caporal
+  .argument('<input-citygml>', 'Input path of CityGML XML file', (path) => {
+    if (!fs.existsSync(path)) {
+      throw new Error('File does not exist: ' + path);
+    }
+    return path;
+  })
+  .argument('<output-3dtiles>', 'Output folder where to create 3D-Tiles')
+  .action(async function(args, options, logger) {
+    let converter = new Converter();
+    logger.info('Converting...');
+    await converter.convertFiles(args['inputCitygml'], args['output3Dtiles']);
+    logger.info('Done.');
+  });
+
+caporal.parse(process.argv);
