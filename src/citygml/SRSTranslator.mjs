a166abfb8b8e9f10b120edf074e526c352723f70
Reto Kaiser <reto@retokaiser.com>
2018-03-25 15:45:38 +0200

    Add SRSTranslator

---
+++ src/citygml/SRSTranslator.mjs
@@ -0,0 +1,93 @@
+import proj4 from 'proj4'
+
+class SRSTranslator {
+
+  /**
+   * @param {Object} [projectionDefinitions]
+   */
+  constructor(projectionDefinitions) {
+    this.projections = {};
+    this.transformations = {};
+
+    projectionDefinitions = Object.assign(
+      SRSTranslator._getDefaultDefinitions(),
+      projectionDefinitions || {}
+    );
+    for (let name in projectionDefinitions) {
+      this.addProjection(name, projectionDefinitions[name]);
+    }
+  }
+
+  /**
+   * @param {String} name
+   * @param {String} projection
+   */
+  addProjection(name, projection) {
+    this.projections[name] = projection;
+  }
+
+  /**
+   * @param {Number[]} coords
+   * @param {String} projectionFrom
+   * @param {String} projectionTo
+   * @return {Number[]}
+   */
+  forward(coords, projectionFrom, projectionTo) {
+    let height = undefined;
+    if (coords.length === 3) {
+      // proj4js doesn't support 'height', just preserve the input value
+      height = coords.pop();
+    }
+
+    let transformation = this._getTransformation(projectionFrom, projectionTo);
+    coords = transformation.forward(coords);
+
+    if (typeof height !== 'undefined') {
+      coords[2] = height;
+    }
+    return coords;
+  }
+
+  /**
+   * @param {String} projectionFrom
+   * @param {String} projectionTo
+   * @return {Function}
+   */
+  _getTransformation(projectionFrom, projectionTo) {
+    let cacheKey = `${projectionFrom}:::${projectionTo}`;
+    if (!this.transformations[cacheKey]) {
+      let from = this._getProjection(projectionFrom);
+      let to = this._getProjection(projectionTo);
+      this.transformations[cacheKey] = proj4(from, to);
+    }
+    return this.transformations[cacheKey];
+  }
+
+  /**
+   * @param {String} name
+   * @return {String}
+   * @private
+   */
+  _getProjection(name) {
+    if (!this.projections[name]) {
+      throw new Error(`Unknown projection name: "${name}"`);
+    }
+    return this.projections[name];
+  }
+
+  /**
+   * @returns {Object}
+   * @private
+   */
+  static _getDefaultDefinitions() {
+    return {
+      'WGS84': '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees',
+      'CH1903+': '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs',
+      'CH1903': '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs',
+      'urn:ogc:def:crs:EPSG::7415': '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +vunits=m +no_defs',
+    };
+  }
+
+}
+
+export default SRSTranslator
---
+++ src/citygml/SRSTranslator.test.mjs
@@ -0,0 +1,46 @@
+import SRSTranslator from "./SRSTranslator.mjs";
+import chai from 'chai';
+import assert from "assert";
+
+describe('SRSTranslator', async function() {
+
+  describe('#forward()', () => {
+    let transformer = new SRSTranslator();
+
+    it('should convert 2D coordinates', () => {
+      chai.assert.deepEqual(
+        transformer.forward([683303.518, 247425.762], 'CH1903', 'WGS84'),
+        [8.541602004702368, 47.37240457906726]
+      );
+    });
+
+    it('should preserve "height" component of 3D coordinates', () => {
+      chai.assert.deepEqual(
+        transformer.forward([683303.518, 247425.762, 300.4], 'CH1903', 'WGS84'),
+        [8.541602004702368, 47.37240457906726, 300.4]
+      );
+    });
+
+    it('should throw on unknown projection', () => {
+      assert.throws(() => {
+        transformer.forward([2, 3], 'XYZ123', 'WGS84');
+      }, /Unknown projection/);
+    });
+  });
+
+  describe('#constructor()', () => {
+    describe('with custom projection definitions', () => {
+      let transformer = new SRSTranslator({
+        'EPSG:5243': '+proj=lcc +lat_1=48.66666666666666 +lat_2=53.66666666666666 +lat_0=51 +lon_0=10.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+      });
+
+      it('should convert from custom projection', () => {
+        chai.assert.deepEqual(
+          transformer.forward([195927.24, 172450.97], 'EPSG:5243', 'WGS84'),
+          [13.388888936536285, 52.5166670295793]
+        );
+      });
+    });
+  });
+
+});
